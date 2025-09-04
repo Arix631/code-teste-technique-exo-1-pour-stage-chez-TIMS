266132984eade93be63c640df65aeb008d6b1a34
Amine <kebaieramine1@gmail.com>
2025-09-04 21:18:33 +0200

    Ajout de l'exercice 1 fizzbuzz

---
+++ main.js
@@ -0,0 +1,19 @@
+
+let chaine1 = "fizz";
+let chaine2 = "buzz";
+
+
+for (let i = 1; i <= 20; i++) {
+
+  if (i % 3 === 0) {
+    console.log(chaine1);
+
+  }
+  else if (i % 5 === 0) {
+    console.log(chaine2);
+  }
+  else {
+    console.log(i);
+  }
+
+}
